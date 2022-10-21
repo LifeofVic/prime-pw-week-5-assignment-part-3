6046655815ab07aabac6e7d74c3ca41207d230e5
Victor Llapa <victorllapa@gmail.com>
2022-10-20 23:58:38 -0500

    Created a showCollection function to display the current number of albums in the collection array, as well as the correct format when console loggin the albums in the array.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,7 +12,7 @@ function addToCollection(title, artist, yearPublished) {
 	console.log('Album added to collection: ', album);
 }
 
-
+console.log('********** Start of addToCollection **********')
 addToCollection('Un Verano Sin Ti', 'Bad Bunny', 2022);
 
 addToCollection('Certified Lover Boy', 'Drake', 2022);
@@ -27,3 +27,25 @@ addToCollection('I Never Liked You', 'Future', 2022);
 
 
 console.log('Entire Albums in Collection: ', collection);
+
+console.log('********** End of addToCollection **********')
+
+console.log('********** Start of showCollection **********')
+
+function showCollection(Array) {
+	console.log('Albums is the current Collection is: ', array.length);
+
+	for (info in collection) {
+		console.log(info.title, 'by ', info.artist, ', published in', info.yearPublished);
+	}
+}
+
+
+
+
+
+
+
+console.log('********** End of showCollection **********')
+
+
